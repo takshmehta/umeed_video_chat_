978bdf13e2b898d0263bcdfc146a1b7515ce44fe
Ridhika2000 <ridhikagarg8@gmail.com>
2021-03-29 07:31:19 -0700

    some changes

--- controller/controller_auth.js
+++ controller/controller_auth.js
@@ -1,15 +1,17 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const expressJwt = require("express-jwt");
-const nodemailer=require('nodemailer');
-const bcrypt=require('bcryptjs');
+const nodemailer = require("nodemailer");
+const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
+const { v4 } = require("uuid");
 
-var transporter=nodemailer.createTransport({
-  service:'gmail',
-  auth:{
-      user:'umeed123890@gmail.com',
-      pass:'qweqwe@123'
-  }
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "umeed123890@gmail.com",
+    pass: "qweqwe@123",
+  },
 });
 
 exports.getUserById = (req, res, next, id) => {
@@ -36,7 +38,6 @@ exports.signup = (req, res) => {
     res.cookie("token", token, { expire: new Date() + 5555 });
     const { _id, name, email } = user;
     return res.json({ token, user: { _id, name, email } });
-    
   });
 };
 
@@ -59,123 +60,109 @@ exports.signin = (req, res) => {
   });
 };
 
-
-exports.forgotpswd=async(req,res)=>{
-  const email=req.body.email;
-  const user=await User.findOne({email:req.body.email});
-  if(user){
+exports.forgotpswd = async (req, res) => {
+  const email = req.body.email;
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
     console.log(user._id);
-      const resetToken = jwt.sign({_id:user._id}, 'asdfghjklqwertyuiop', {expiresIn:'5m'});
-      console.log(resetToken);
-      var mailOptions={
-          from:'umeed123890@gmail.com',
-          to:email,
-          subject:'Reset passsword',
-          html:`
+    const resetToken = jwt.sign({ _id: user._id }, "asdfghjklqwertyuiop", {
+      expiresIn: "300000",
+    });
+    console.log(resetToken);
+    var mailOptions = {
+      from: "umeed123890@gmail.com",
+      to: email,
+      subject: "Reset passsword",
+      html: `
           <p>You requested for reset password</p>
          
             <h2> Please click on this <a href="http://localhost:3001/reset-password/${req.body.email}/${resetToken}">http://new-database/_/auth/reset_pwd/${req.body.email}?token=${resetToken}</a> link for reset password- </h2>;
           
          
-          <br/>`,    }
-          transporter.sendMail(mailOptions,function(error,info){
-              if(error){
-                  console.log(error);
-              }
-              else{
-                 
-                  res.status(200).json({
-                      message:`Reset email sent to ${email} successfully! Please check your mail for further instructions`
-                  })
-              };
-
-                 
-              })
-          
-
-          }
-
-
-
-          else{
-              res.status(401).json({
-                  message:`Profile of ${email} not found!`
-              })
-          }
-  
-
-  
-
-
-      
-
+          <br/>`,
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        return res.status(200).json({
+          message: `Reset email sent to ${email} successfully! Please check your mail for further instructions`,
+        });
+      }
+    });
+  } else {
+    return res.status(401).json({
+      message: `Profile of ${email} not found!`,
+    });
   }
+};
 
-
-  exports.resetPassword=async (req,res)=>{
-      const resetToken=req.params.resettoken;
-      const user=await User.findOne({email:req.params.email});
-console.log(req.params.email);
-console.log(req.body.newpswd);
-console.log(req.body.cnfrmnewpswd);
-console.log(resetToken);
-      if(resetToken){
-          jwt.verify(resetToken,'asdfghjklqwertyuiop', (err,user)=>{
-              if(err){
-                  if(err.name=="TokenExpiredError"){
-                      res.status(420).json({
-                          error:"Token timed out!",
-                          
-                      })
-                  }
-                  else{
-                      res.status(421).json({
-                          error:"Incorrect Token"
-                      })
-                  }
+exports.resetPassword = async (req, res) => {
+  const resetToken = req.params.resettoken;
+  var user1 = User.findOne({ email: req.params.email });
+  // console.log(req.params.email);
+  // console.log(req.body.newpswd);
+  // console.log(req.body.cnfrmnewpswd);
+  // console.log(resetToken);
+  if (resetToken) {
+    jwt.verify(resetToken, "asdfghjklqwertyuiop", (err, user) => {
+      if (err) {
+        if (err.name == "TokenExpiredError") {
+          console.log(err);
+          return res.status(420).json({
+            error: "Token timed out!",
+          });
+        } else {
+          return res.status(421).json({
+            error: "Incorrect Token",
+          });
+        }
+      } else {
+        console.log(user);
+        if (req.body.newpswd === req.body.cnfrmnewpswd) {
+          const newSalt = v4();
+          const encry_password = crypto
+            .createHmac("sha256", newSalt)
+            .update(req.body.newpswd)
+            .digest("hex");
+          User.findOneAndUpdate(
+            { _id: user._id },
+            { encry_password: encry_password, salt: newSalt },
+            { new: true }
+          )
+            .then((user) => {
+              // console.log(user);
+              if (user) {
+                return res.status(200).json({
+                  message: `Password of ${user.email} changed successfully!`,
+                });
+              } else {
+                return res.status(401).json({
+                  error: `Profile of 1'${user._id} not found!`,
+                });
               }
-              else{
-                  if(req.body.newpswd===req.body.cnfrmnewpswd){
-                  // bcrypt.hash(req.body.newpswd,8)
-                  // .then((hashedPassword)=>{
-                      return User.findOneAndUpdate({_id:user._id},{password:req.body.newpswd},{new:true})
-                  // })
-                  .then((user)=>{
-                      if(user){
-                          res.status(200).json({
-                              message:`Password of ${user.email} changed successfully!`
-                          })
-                      }
-                      else{
-                          res.status(401).json({
-                              error:`Profile of 1'${user._id} not found!`
-                          })
-                      }
-                  })
-                  .catch((err)=>{
-                      res.status(400).json({
-                          error:`Error updating  password!`
-                      })
-                  })  
-              }
-          else{
-              res.status(402).json({
-                  error:"password not matching"
-              })
-          }
+            })
+            .catch((err) => {
+              console.log(err);
+              return res.status(400).json({
+                error: `Error updating  password!`,
+              });
+            });
+          // );
+        } else {
+          return res.status(402).json({
+            error: "password not matching",
+          });
+        }
       }
-
-          })
-      }
-      else{
-      
-          res.status(405).json({
-              error:'Authentication token not found!'
-          })
-      }
+    });
+  } else {
+    return res.status(405).json({
+      error: "Authentication token not found!",
+    });
   }
+};
 
-  
 exports.signout = (req, res) => {
   res.clearCookie("token");
   return res.json({ message: "USer signed out" });
