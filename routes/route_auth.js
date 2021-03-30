162cb56d71a91bc396ee0c7a4dbc15cd5a702b94
taksh mehta <takshmehta111@gmail.com>
2021-03-30 19:15:51 +0530

    For implementing validation on various routes

--- routes/route_auth.js
+++ routes/route_auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Router = express.Router();
-
+const { check } = require("express-validator");
 const {
   signup,
   signin,
@@ -14,11 +14,42 @@ const {
 
 Router.param("userId", getUserById);
 
-Router.post("/signup", signup);
-Router.post("/signin", signin);
+Router.post(
+  "/signup",
+  [
+    check("name", "Name should be more than 3 letters").isLength({ min: 3 }),
+    check("email", "Please enter a valid email.").isEmail(),
+    check("password", "Password should contain atleast 5 letters.").isLength({
+      min: 5,
+    }),
+  ],
+  signup
+);
+Router.post(
+  "/signin",
+  [
+    check("email", "Please enter a valid email.").isEmail(),
+    check("password", "Password should contain atleast 5 letters.").isLength({
+      min: 5,
+    }),
+  ],
+  signin
+);
 Router.get("/signout", signout);
-Router.post("/forgotpswd",forgotpswd);
-Router.put("/resetpswd/:email/:resettoken",resetPassword);
+Router.post(
+  "/forgotpswd",
+  [check("email", "Please enter a valid email.").isEmail()],
+  forgotpswd
+);
+Router.put(
+  "/resetpswd/:email/:resettoken",
+  [
+    check("newpswd", "Password should contain atleast 5 letters.").isLength({
+      min: 5,
+    }),
+  ],
+  resetPassword
+);
 
 Router.get("/testroute/:userId", isSignedIn, isAuthenticated, (req, res) => {
   res.json({ success: "wokred" });
