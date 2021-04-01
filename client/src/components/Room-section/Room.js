bdad8118d88ae0118e0584f3bda6653def902db2
taksh mehta <takshmehta111@gmail.com>
2021-04-01 19:04:32 +0530

    max user popup

--- client/src/components/Room-section/Room.js
+++ client/src/components/Room-section/Room.js
@@ -2,12 +2,14 @@ import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import Peer from "simple-peer";
 import styled from "styled-components";
-import './Dashboard.css'
+import { toast, ToastContainer, Zoom } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import "./Dashboard.css";
 const Container = styled.div`
   padding: 20px;
   display: flex;
   height: 100vh;
-  background:#111;
+  background: #111;
   margin: auto;
   flex-wrap: wrap;
 `;
@@ -15,8 +17,6 @@ const Container = styled.div`
 const StyledVideo = styled.video`
   height: 40%;
   width: 50%;
-
- 
 `;
 
 const Video = (props) => {
@@ -28,7 +28,9 @@ const Video = (props) => {
     });
   }, []);
 
-  return <StyledVideo playsInline autoPlay ref={ref} className="bg-video-tag"  />;
+  return (
+    <StyledVideo playsInline autoPlay ref={ref} className="bg-video-tag" />
+  );
 };
 
 const videoConstraints = {
@@ -48,7 +50,9 @@ const Room = (props) => {
     navigator.mediaDevices
       .getUserMedia({ video: videoConstraints, audio: true })
       .then((stream) => {
+        stream.muted = true;
         userVideo.current.srcObject = stream;
+
         socketRef.current.emit("join room", roomID);
         socketRef.current.on("all users", (users) => {
           const peers = [];
@@ -58,7 +62,7 @@ const Room = (props) => {
               peerID: userID,
               peer,
             });
-            peers.push({peerID:userID,peer});
+            peers.push({ peerID: userID, peer });
           });
           setPeers(peers);
         });
@@ -69,9 +73,8 @@ const Room = (props) => {
             peerID: payload.callerID,
             peer,
           });
-         
-          setPeers( [ ...peersRef.current ]); 
-          
+
+          setPeers([...peersRef.current]);
         });
 
         socketRef.current.on("receiving returned signal", (payload) => {
@@ -79,16 +82,15 @@ const Room = (props) => {
           item.peer.signal(payload.signal);
         });
 
-        socketRef.current.on("user left",id=>{
-          const peerObj=peersRef.current.find(p=> p.peerID===id);
-          if(peerObj){
-              peerObj.peer.destroy();
+        socketRef.current.on("user left", (id) => {
+          const peerObj = peersRef.current.find((p) => p.peerID === id);
+          if (peerObj) {
+            peerObj.peer.destroy();
           }
-          const peers=peersRef.current.filter(p=> p.peerID!==id);
-          peersRef.current=peers;
+          const peers = peersRef.current.filter((p) => p.peerID !== id);
+          peersRef.current = peers;
           setPeers(peers);
-          })
-
+        });
       });
   }, []);
 
@@ -126,55 +128,64 @@ const Room = (props) => {
     return peer;
   }
   const videoToggle = () => {
-
     let enabled = userVideo.current.srcObject.getVideoTracks()[0].enabled;
-    
+
     if (enabled) {
-      let videoElement=document.querySelector('.fa-video');
-      videoElement.classList.remove('fa-video');
-      videoElement.classList.add('fa-video-slash');
+      let videoElement = document.querySelector(".fa-video");
+      videoElement.classList.remove("fa-video");
+      videoElement.classList.add("fa-video-slash");
       userVideo.current.srcObject.getVideoTracks()[0].enabled = false;
     } else {
-      let videoElement=document.querySelector('.fa-video-slash');
-      videoElement.classList.remove('fa-video-slash');
-      videoElement.classList.add('fa-video');
+      let videoElement = document.querySelector(".fa-video-slash");
+      videoElement.classList.remove("fa-video-slash");
+      videoElement.classList.add("fa-video");
       userVideo.current.srcObject.getVideoTracks()[0].enabled = true;
     }
   };
-  const audioToggle = () =>{
+  const audioToggle = () => {
     let enabled = userVideo.current.srcObject.getAudioTracks()[0].enabled;
     if (enabled) {
-      let audioElement=document.querySelector('.fa-microphone');
-      audioElement.classList.remove('fa-microphone');
-      audioElement.classList.add('fa-microphone-slash');
+      let audioElement = document.querySelector(".fa-microphone");
+      audioElement.classList.remove("fa-microphone");
+      audioElement.classList.add("fa-microphone-slash");
       userVideo.current.srcObject.getAudioTracks()[0].enabled = false;
     } else {
-      let audioElement=document.querySelector('.fa-microphone-slash');
-      audioElement.classList.remove('fa-microphone-slash');
-      audioElement.classList.add('fa-microphone');
+      let audioElement = document.querySelector(".fa-microphone-slash");
+      audioElement.classList.remove("fa-microphone-slash");
+      audioElement.classList.add("fa-microphone");
       userVideo.current.srcObject.getAudioTracks()[0].enabled = true;
     }
   };
   console.log(peers);
   return (
     <Container>
-      <StyledVideo  ref={userVideo} autoPlay playsInline   />
+      <ToastContainer position="top-right" />
+
+      <StyledVideo ref={userVideo} autoPlay playsInline muted />
       {peers.map((peer) => {
-        return <Video key={peer.peerID} peer={peer.peer}  />;
+        return <Video key={peer.peerID} peer={peer.peer} />;
       })}
-      <div className='footer'>
+      {peers.length === 3
+        ? toast("Max user limit reached!!", {
+            type: "warning",
+            autoClose: 3000,
+            pauseOnHover: false,
+            transition: Zoom,
+          })
+        : null}
+      <div className="footer">
         <div>
-        <button onClick={audioToggle}>
-           <i className="fas fa-microphone"></i>
-        </button>
-        <button>
-           <a href='/'>
+          <button onClick={audioToggle}>
+            <i className="fas fa-microphone"></i>
+          </button>
+          <button>
+            <a href="/">
               <i className="fas fa-phone-alt leave-icon"></i>
-           </a>
-        </button>
-        <button onClick={videoToggle}>
-            <i className="fas fa-video"></i> 
-        </button>
+            </a>
+          </button>
+          <button onClick={videoToggle}>
+            <i className="fas fa-video"></i>
+          </button>
         </div>
       </div>
     </Container>
