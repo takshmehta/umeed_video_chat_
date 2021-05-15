f7e59176ec28b1e62238d56863bd30476eb472a3
taksh mehta <takshmehta111@gmail.com>
2021-05-16 01:08:19 +0530

    Ui changes

--- client/src/components/Home-section/Nav-layout/Error.js
+++ client/src/components/Home-section/Nav-layout/Error.js
@@ -13,9 +13,9 @@ const Error = () => {
               </div>
 
               <div class="contant_box_404">
-                <h3 class="h2">Look like you're lost</h3>
+                <h3 class="h2">Looks like you're lost</h3>
 
-                <p>the page you are looking for not avaible!</p>
+                <p>The page you are looking for is not available!</p>
 
                 <Link to="/" className="link_404">
                   Go to Home
